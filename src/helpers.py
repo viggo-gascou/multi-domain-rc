8c83c7b273270fd26fbed8581b686899aca8d941
viggo-gascou <viggo.gascou@gmail.com>
2023-05-04 09:21:16 +0000

    helpers aso

--- src/helpers.py
+++ src/helpers.py
@@ -1,8 +1,8 @@
 import pandas as pd
-from deepsig import multi_aso
+from deepsig import aso
 
 
 def significance_test(
     model_scores: dict[str, list[float]], seed: int = 1234
 ) -> pd.DataFrame:
-    return multi_aso(my_models_scores, confidence_level=0.95, return_df=True, seed=seed)
+    return aso(my_models_scores, confidence_level=0.95, return_df=True, seed=seed)
