041697f39adf6b2e1477a4ea0c1e9ee4e62a62d7
Gustav Kristensen <gustavkrist@gmail.com>
2023-10-09 13:05:00 +0200

    Removed unused code and syp-related files

--- src/helpers.py
+++ src/helpers.py
@@ -1,15 +1,5 @@
-import pandas as pd
-from deepsig import aso
-
-
-def significance_test(
-    model_scores: dict[str, list[float]], seed: int = 1234
-) -> pd.DataFrame:
-    return aso(my_models_scores, confidence_level=0.95, return_df=True, seed=seed)
-
-
-# (i.e. person, location, organisation, misc, event)
-
+# Mapping to 5 entity types present in all domains
+# (person, location, organisation, misc, event)
 
 entity_to_generic = {
     "academicjournal": "misc",
