2b48cdff70c209648989cbcabddef5765db39eec
robvanderg <robvanderg@live.nl>
2023-10-11 11:42:09 +0200

    Added code for plots

---
+++ plots/scatterplot-domains.py
@@ -0,0 +1,65 @@
+import json
+import os
+import matplotlib as mpl
+import matplotlib.patches as mpatches
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+plt.style.use('rob.mplstyle')
+
+import torch
+from transformers import AutoModel
+from transformers import AutoTokenizer
+from sklearn.decomposition import PCA
+
+data_dir = 'crossre_data/'
+
+all_embeddings = []
+domain_labels = []
+
+lm = 'bert-base-cased'
+lang_model = AutoModel.from_pretrained(lm)
+tokenizer = AutoTokenizer.from_pretrained(lm)
+
+for annotated_file in os.listdir(data_dir):
+    if 'train' in  annotated_file:
+        print('reading and encoding: ' + annotated_file)
+        domain = annotated_file.split('-')[0]
+        for line in open(data_dir + annotated_file):#.readlines()[:10]:
+            data = json.loads(line)
+            text = ' '.join(data['sentence'])
+            word_ids = tokenizer.encode(text, return_tensors='pt')
+            embeds = lang_model.forward(word_ids)[0]
+            # shape = 1, sent_len, 768
+            # we need to average over middle dimension (words)
+            avg_pooled = embeds.mean(dim=1).squeeze()
+            all_embeddings.append(avg_pooled.detach().numpy())
+            domain_labels.append(domain)
+
+print("PCAing")
+pca = PCA(n_components=2)
+transformed = pca.fit_transform(all_embeddings)
+
+
+fig, ax = plt.subplots(figsize=(8,5), dpi=300)
+domains = sorted(list(set(domain_labels)))
+domain2color = {}
+colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+for domainIdx, domain in enumerate(domains):
+    domain2color[domain] = colors[domainIdx]
+
+
+# adds 1 by 1, perhaps not neatest?
+domainsAdded = set()
+for instanceIdx in range(len(all_embeddings)):
+    coordinate = transformed[instanceIdx]
+    domain = domain_labels[instanceIdx]
+    if domain not in domainsAdded:
+        ax.scatter(coordinate[0], coordinate[1], color=domain2color[domain], label=domain, alpha=.7)
+        domainsAdded.add(domain)
+    else:
+        ax.scatter(coordinate[0], coordinate[1], color=domain2color[domain], alpha=.7)
+    
+leg = ax.legend()
+leg.get_frame().set_linewidth(1.5)
+fig.savefig('scatter_plot_domains.pdf', bbox_inches='tight')
+
---
+++ plots/scatterplot-relations.py
@@ -0,0 +1,84 @@
+import json
+import os
+import matplotlib as mpl
+import matplotlib.patches as mpatches
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+plt.style.use('rob.mplstyle')
+
+import torch
+from sklearn.decomposition import PCA
+
+import csv
+data = []
+all_embeddings = []
+with open("embeddings_relations.csv", 'r') as file:
+    csvreader = csv.reader(file, delimiter=';')
+    for row in csvreader:
+        if row[0] =='embeddings':
+            continue
+        representation = [float(x) for x in row[0].replace('[', '').replace(']', '').split(', ')]
+        all_embeddings.append(representation)
+        data.append(row[1:])
+
+print("PCAing")
+pca = PCA(n_components=2)
+transformed = pca.fit_transform(all_embeddings)
+
+fig, ax = plt.subplots(figsize=(15,12), dpi=300)
+
+domain2marker = {}
+markers = ['.', 'x', '4', '3', '_']
+markers = markers + markers + markers + markers
+domains = sorted(list(set([x[1] for x in data])))
+for domainIdx, domain in enumerate(domains):
+    domain2marker[domain] = markers[domainIdx]
+
+rel2color = {}
+colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+colors = colors + colors + colors
+relation_labels = sorted(list(set([x[0] for x in data])))
+for relIdx, rel in enumerate(relation_labels):
+    rel2color[rel] = colors[relIdx]
+print(len(relation_labels))
+
+freqs = {}
+for item in data:
+    label = item[0]
+    if label in freqs:
+        freqs[label] += 1
+    else:
+        freqs[label] = 1
+
+topN = [x[0] for x in sorted(freqs.items(), key=lambda x:x[1])[-8:]]
+
+domainsAdded = set()
+relsAdded = set()
+for instanceIdx in range(len(all_embeddings)):
+    coordinate = transformed[instanceIdx]
+    relation, domain = data[instanceIdx]
+    if  relation not in topN:
+        continue
+    color = rel2color[relation]
+    marker = domain2marker[domain]
+    if relation not in relsAdded:
+        ax.scatter(coordinate[0], coordinate[1], color=color, marker=marker, label=relation, alpha=.7)
+        relsAdded.add(relation)
+    else:
+        if relation == relation_labels[0]:
+            if domain not in domainsAdded:
+                ax.scatter(coordinate[0], coordinate[1], color=color, marker=marker, label=domain, alpha=.7)
+                domainsAdded.add(domain)
+            else:
+                ax.scatter(coordinate[0], coordinate[1], color=color, marker=marker, alpha=.7)
+        else:
+            ax.scatter(coordinate[0], coordinate[1], color=color, marker=marker, alpha=.7)
+    #if domain not in domainsAdded:
+    #    domainsAdded.add(domain)
+    #else:
+    #    ax.scatter(coordinate[0], coordinate[1], color=domain2color[domain], alpha=.7)
+    
+leg = ax.legend()
+leg.get_frame().set_linewidth(1.5)
+fig.savefig('scatter_plot_relations.pdf', bbox_inches='tight')
+
